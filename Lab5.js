7eaac44100ec2a6e186e09bf0cb7b5f840870b30
William Zhang <wzhang12321@gmail.com>
2024-03-28 01:52:56 -0400

    finished 3.2

--- Lab5.js
+++ Lab5.js
@@ -6,6 +6,13 @@ const assignment = {
   completed: false,
   score: 0,
 };
+
+const module = {
+  id: 4550,
+  name: "Web Development",
+  description: "Learn about web development",
+  course: "CS4550",
+};
 const Lab5 = (app) => {
   app.get("/a5/assignment", (req, res) => {
     res.json(assignment);
@@ -13,6 +20,14 @@ const Lab5 = (app) => {
 
   app.get("/a5/assignment/title", (req, res) => {
     res.json(assignment.title);
+  });
+
+  app.get("a5/module", (req, res) => {
+    res.json(module);
+  });
+
+  app.get("/a5/module/name", (req, res) => {
+    res.json(module.name);
   });
 
   app.get("/a5/add/:a/:b", (req, res) => {
@@ -51,6 +66,21 @@ const Lab5 = (app) => {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
+  });
+  app.get("/a5/assignment/score/:newScore", (req, res) => {
+    const { newScore } = req.params;
+    assignment.score = newScore;
+    res.json(assignment);
+  });
+  app.get("/a5/assignment/completed/:newCompleted", (req, res) => {
+    const { newCompleted } = req.params;
+    assignment.completed = newCompleted;
+    res.json(assignment);
+  });
+  app.get("/a5/module/name/:newName", (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
   });
 };
 

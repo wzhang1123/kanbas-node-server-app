e91196b9b50ccd5b3ec31ed1dea45da250eb8fb4
William Zhang <wzhang12321@gmail.com>
2024-03-29 15:21:32 -0400

    finished 4

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import express from "express";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "../kanbas/src/Kanbas/Courses/routes.js";
+import ModuleRoutes from "../kanbas/src/Kanbas/Courses/Modules/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 app.listen(4000);

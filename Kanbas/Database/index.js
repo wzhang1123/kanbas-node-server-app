3050c9757b8f71c3df7f7530c2e183d1c2b66e55
William Zhang <wzhang12321@gmail.com>
2024-04-01 00:38:58 -0400

    major changes

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,12 @@
+// import courses from "./courses.json";
+// import modules from "./modules.json";
+// import assignments from "./assignments.json";
+// export { courses, modules, assignments };
+
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignments from "./assignments.js";
+import users from "./users.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+export default { courses, modules, assignments, users, grades, enrollments };

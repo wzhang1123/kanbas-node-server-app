54ec801b77d3c50add73be7ac990defe166fc601
William Zhang <wzhang12321@gmail.com>
2024-04-13 17:19:36 -0400

    3.5.2

--- Users/dao.js
+++ Users/dao.js
@@ -1,11 +1,11 @@
 import model from "./model.js";
 export const createUser = (user) => model.create(user);
 export const findAllUsers = () => model.find();
-export const findUserByID = (userID) => model.findUserByID(userID);
+export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>
-  model.findOne({ userName: username });
+  model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>
   model.findOne({ username, password });
-export const updateUser = (userID, user) =>
-  model.updateOne({ _id: userID }, { $set: user });
-export const deleteUser = (userID) => model.deleteOne({ _id: userID });
+export const updateUser = (userId, user) =>
+  model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });

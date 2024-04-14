c79c273cb36e914a21b70c638dd44c67633dafb9
William Zhang <wzhang12321@gmail.com>
2024-04-14 00:59:56 -0400

    3.5.6

--- Users/routes.js
+++ Users/routes.js
@@ -6,7 +6,10 @@ export default function UserRoutes(app) {
     res.json(user);
   };
   app.post("/api/users", createUser);
-  const deleteUser = async (req, res) => {};
+  const deleteUser = async (req, res) => {
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
   const findAllUsers = async (req, res) => {
     const users = await dao.findAllUsers();
     res.json(users);

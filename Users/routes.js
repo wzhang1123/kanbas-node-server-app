da569e6e916db0337df7d083313f8027b75dc679
William Zhang <wzhang12321@gmail.com>
2024-04-14 00:42:01 -0400

    3.5.5

--- Users/routes.js
+++ Users/routes.js
@@ -1,11 +1,24 @@
 import * as dao from "./dao.js";
 let currentUser = null;
 export default function UserRoutes(app) {
-  const createUser = async (req, res) => {};
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
+  app.post("/api/users", createUser);
   const deleteUser = async (req, res) => {};
-  const findAllUsers = async (req, res) => {};
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+  app.get("/api/users", findAllUsers);
   const findUserById = async (req, res) => {};
-  const updateUser = async (req, res) => {};
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
   const signup = async (req, res) => {};
   const signin = async (req, res) => {
     const { username, password } = req.body;
